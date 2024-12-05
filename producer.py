0a4d6852c71bb94e5b546a2c8c99926d2c45236a
Ruslan <79635186+333nova@users.noreply.github.com>
2024-12-05 15:44:55 +0100

    Update producer.py
    seperation into 2 topics
    wind value added

--- producer.py
+++ producer.py
@@ -8,6 +8,7 @@ from confluent_kafka import Producer
 date_stamp = datetime.datetime(2023, 1, 1, 0, 0)
 temperature_value = 2
 humidity_value = 75
+wind_value = 15
 uv_value = 0
 
 # Kafka configuration
@@ -29,17 +30,15 @@ def read_config():
         config[parameter] = value.strip()
   return config
 
-# Data Generation
-def generate_log_entry():
-    global date_stamp, temperature_value, humidity_value, uv_value  
+      # DATA GENERATION
+
+def generate_topic1():
+    global date_stamp, temperature_value, humidity_value
     start_night = datetime.time(19, 0)  
     end_night = datetime.time(7, 0)    
     
     #values generation, first night
     if date_stamp.time() >= start_night or date_stamp.time() < end_night:   #night
-        #uv
-        uv_value = 0
-
         #temperature
         perturbation_temp = int(random.uniform(-5, 1))
         new_temperature_value = temperature_value + perturbation_temp
@@ -53,15 +52,10 @@ def generate_log_entry():
     else:      #day                                                 
         #uv and temperature, we seperate the day in two parts: before 2am they rise and after they progressivly fall 
         if date_stamp.time() <= datetime.time(14,0):
-            uv_value += 1 if random.random() < 0.4 else 0      #40% chance to rise so it doesnt rise too fast
-
             perturbation_temp = int(random.uniform(-1, 4))
             new_temperature_value = temperature_value + perturbation_temp
             temperature_value = max (2, min(12, new_temperature_value))
         else:
-            new_uv_value = uv_value - uv_value/ 7                #so it declines progressivly towards 0
-            uv_value = new_uv_value
-
             new_temp_value = temperature_value - temperature_value / 7
             temperature_value = new_temp_value
 
@@ -74,31 +68,66 @@ def generate_log_entry():
        "datestamp": date_stamp.isoformat(),
        "temperature": int(temperature_value),
         "humidity": humidity_value,
+    }
+    return log_entry
+
+def generate_topic2():
+    global date_stamp, uv_value, wind_value  
+    start_night = datetime.time(19, 0)  
+    end_night = datetime.time(7, 0)    
+    
+    #values generation, first night
+    if date_stamp.time() >= start_night or date_stamp.time() < end_night:   #night
+        #uv
+        uv_value = 0
+
+    else:      #day                                                 
+        #uv and temperature, we seperate the day in two parts: before 2am they rise and after they progressivly fall 
+        if date_stamp.time() <= datetime.time(14,0):
+            uv_value += 1 if random.random() < 0.4 else 0      #40% chance to rise so it doesnt rise too fast
+        else:
+            new_uv_value = uv_value - uv_value/ 7                #so it declines progressivly towards 0
+            uv_value = new_uv_value
+    
+    if wind_value <= 9:                                #when approaching the limits (4-34km/h), lower probability to gow lower/higher
+      perturbation_wind = int(random.uniform(-2, 5))
+    elif wind_value >= 29:
+      perturbation_wind = int(random.uniform(-5, 1))
+    else:
+      perturbation_wind = int(random.uniform(-3, 3))
+    new_wind_value = wind_value + perturbation_wind
+    wind_value = max (4, min(34, new_wind_value))
+
+    log_entry = {
+        "datestamp": date_stamp.isoformat(),
         "uv": int(uv_value),
+        "wind": int(wind_value)
     }
     
     date_stamp += datetime.timedelta(hours=1)
-
     return log_entry
-"""
-for i in range(24):
-    print(generate_log_entry())
-"""
+
+
+    # PRODUCER
 
 def produce(topic, config):
   # creates a new producer instance
   producer = Producer(config)
 
   while True:
-    log_entry = generate_log_entry()
+    log_entry = generate_topic1()          #topic 2
     # Convert log entry to JSON format
     log_entry_json = json.dumps(log_entry)
-        
     # Produce message to Kafka
     producer.produce(topic, key=log_entry["datestamp"], value=log_entry_json)
     print(f"Produced message to topic {topic}: {log_entry_json}")
-
     # send any outstanding or buffered messages to the Kafka broker
+    producer.flush()
+
+    log_entry = generate_topic2()            #topic 1
+    log_entry_json = json.dumps(log_entry)
+    producer.produce(topic, key=log_entry["datestamp"], value=log_entry_json)
+    print(f"Produced message to topic {topic}: {log_entry_json}")
     producer.flush()
 
     # Random delay between 0.1 and 1 second to simulate 1-10 messages per second
